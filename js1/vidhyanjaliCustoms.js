57544dcfea0b75a380fc57e3d600919acdc76652
Ravi Kiran Lingam <lravikiran@icloud.com>
2020-02-04 13:49:15 +0530

    More changes in images and directors page

--- js1/vidhyanjaliCustoms.js
+++ js1/vidhyanjaliCustoms.js
@@ -13,12 +13,13 @@
       // Stop the running slider before hiding it
       $('#eventsCarousels .arrow1.active .vi_slider').data('nivoslider').stop();
       // Hide all elements except the desired one
-      $('#eventsCarousels .arrow1').removeClass('active');
-      $('#eventsCarousels .arrow1:eq('+ parentElementNo +')').addClass('active').find('.vi_slider').nivoSlider();
+      $('#eventsCarousels .arrow1').removeClass('active').addClass('hide');
+      $('#eventsCarousels .arrow1:eq('+ parentElementNo +')').removeClass('hide').addClass('active').find('.vi_slider').nivoSlider();
     });
   };
   swtichSlider();
   // On page load start the default slider for active arrow box.
-  $('#eventsCarousels .arrow1.active .vi_slider').nivoSlider();
+  $('#eventsCarousels .arrow1').addClass('hide');
+  $('#eventsCarousels .arrow1.active .vi_slider').removeClass('hide').nivoSlider();
   
 })(jQuery);
